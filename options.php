1633a6ec98cfbe1aaa000cc16b81154387ece0f4
Nick Sergeevich <nikolays93@ya.ru>
2021-03-29 22:52:02 +0400

    Module options

--- options.php
+++ options.php
@@ -1,32 +1,38 @@
 <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
+use Bitrix\Main\Config\Option;
 use Bitrix\Main\Localization\Loc;
 
 Loc::loadMessages(__FILE__);
 
 /** @var string */
 $moduleId = 'contact.forms';
+/** @var array  */
+$arModuleOptions = Option::getForModule($moduleId, SITE_ID);
 /** @var array */
 $aTabs = array(
     array(
-        'DIV' => 'Captcha',
+        'DIV' => 'SPAM_PROTECTION',
         'TAB' => Loc::getMessage('CONTACT_FORMS_TAB_SPAM_PROTECTION'),
         'OPTIONS' => array(
-            array(
-                'CAPTCHA_ENABLED',
-                Loc::getMessage('CONTACT_FORMS_PARAM_CAPTCHA_ENABLED_TEXT'),
-                null,
-                array('checkbox', 0, '', 'N', '', 'N')
-            ),
+            Loc::getMessage('CONTACT_FORMS_TITLE_CAPTCHA'),
             array(
                 'CAPTCHA_TYPE',
                 Loc::getMessage('CONTACT_FORMS_PARAM_CAPTCHA_TYPE_TEXT'),
                 null,
                 array('selectbox', array(
-                    'bitrix' => 'Bitrix built in',
-                    'grecaptcha' => 'Google reCaptcha',
+                    'NONE' => Loc::getMessage('CONTACT_FORMS_PARAM_CAPTCHA_TYPE_NONE_OPTION'),
+                    'BITRIX' => Loc::getMessage('CONTACT_FORMS_PARAM_CAPTCHA_TYPE_BITRIX_OPTION'),
+                    'GRECAPTCHA' => Loc::getMessage('CONTACT_FORMS_PARAM_CAPTCHA_TYPE_GRECAPTCHA_OPTION'),
                 )),
-            )
+            ),
+            array(
+                'CAPTCHA_SECRET_KEY',
+                Loc::getMessage('CONTACT_FORMS_PARAM_CAPTCHA_SECRET_KEY_TEXT'),
+                null,
+                array('text', 52),
+            ),
+            array('note' => Loc::getMessage('CONTACT_FORMS_PARAM_CAPTCHA_SECRET_KEY_TIP')),
         ),
     ),
 );
